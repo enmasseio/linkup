c35a95aac6a4f0a5679436da8f8753d6c8b7b965
jos <wjosdejong@gmail.com>
2016-03-01 11:23:01 +0100

    Heroku websocket experiment

--- src/peer/hookup.js
+++ src/peer/hookup.js
@@ -1,12 +1,12 @@
 import debug from 'debug/browser';
 import Peer from './Peer';
 
-const BROKER_URL = 'ws://hookitup.herokuapp.com';
+const BROKER_URL = 'wss://hookitup.herokuapp.com';
 
 /**
  * Create a new Peer
  * @param {string} id
- * @param {string} [brokerUrl='ws://localhost:3000']
+ * @param {string} [brokerUrl='wss://hookitup.herokuapp.com']
  * @return {Peer}
  */
 export function createPeer (id, brokerUrl = BROKER_URL) {
